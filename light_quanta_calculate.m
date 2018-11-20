5e95e8356b0c5b6482b2ab869aa197a31cd919c5
So-shi <king.eskey@gmail.com>
2018-11-20 23:20:01 +0900

    球内の乱数の直線を追加

--- light_quanta_calculate.m
+++ light_quanta_calculate.m
@@ -6,11 +6,11 @@
 %葉モデルの面の頂点座標を決定
 
 %太陽の動きを15分おきに更新。
-%中心座標をプロット
-figure;
 
-[x, y, z] = sph2cart(deg2rad(sunposition.Azimuth),deg2rad(sunposition.Elevation),10.0);
-plot3(x, y, z, "o")
+%太陽の中心座標をプロット
+figure;
+[sun_x, sun_y, sun_z] = sph2cart(deg2rad(sunposition.Azimuth),deg2rad(sunposition.Elevation),10.0);
+plot3(sun_x, sun_y, sun_z, "o")
 xlabel("x (east or west)")
 ylabel("y (north or south)")
 zlabel("z (height)")
@@ -18,29 +18,37 @@ grid on;
 hold on;
 
 
-
 %球の乱数を生成
 %figure
-for n = 1:2
+tmp_rand = 10;  %乱数を生成する数。後々、日射量に比例して設定するので、一時的なやつ
+for n = 10:50
     rng(0,'twister')
-    rvals = 2*rand(100,1)-1;
+    rvals = 2*rand(tmp_rand,1)-1;
     elevation = asin(rvals);
 
-    azimuth = 2*pi*rand(100,1);
-    radii = 1*(rand(100,1).^(1/3));
+    azimuth = 2*pi*rand(tmp_rand,1);
+    radii = 1*(rand(tmp_rand,1).^(1/3));
     [x_rand,y_rand,z_rand] = sph2cart(azimuth,elevation,radii);
-    plot3(x_rand + x(n), y_rand + y(n), z_rand + z(n),'.')
+    plot3(x_rand + sun_x(n), y_rand + sun_y(n), z_rand + sun_z(n),'.');
     hold on
     
     %乱数で生成した点の直線の描画
-    direction_vector = [x(n), y(n), z(n)];   %方向ベクトルの保持
-    y_tmp = line_xz(x_rand, z_rand);
-    disp(direction_vector);
+    direction_vector = [sun_x(n), sun_y(n), sun_z(n)];  %方向ベクトルの保持
+    line_rand(x_rand, y_rand, z_rand, direction_vector);  %ここをコメントしたら一応軽くなる。
     
 end
 
-function y = line_xz(x, z)
-   y = x+z;
+%球内で生成した点を通る直線の描画
+function line_rand(x_rand, y_rand, z_rand, direction_vector)
+    t = 0:1;
+    
+    for n = 1:length(x_rand)
+        %if z_rand(n) + t*direction_vector(3) >= 0
+            plot3(x_rand() + t*direction_vector(1), y_rand(n) + t*direction_vector(2),...
+                z_rand(n) + t*direction_vector(3));
+            hold on
+        %end
+    end
 end
 
 %模擬放射光源の作成（試作段階）
